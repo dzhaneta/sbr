43be8e43e27ed44c3ae851446cc14e33d3d78008
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2023-10-12 12:25:13 +0300

    task 1: resolved

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,48 @@
-import {encoded, translations} from './data.js'
+import {encoded, translations} from './data.js';
 
-console.log("Let's rock")
-console.log(encoded, translations)
+console.log("Let's rock");
+// console.log(encoded, translations)
 
+const excluded = ['groupId', 'service', 'formatSize', 'ca'];
+let uniqueIds = []; // unique ids list
 
+function decode(data, encryption) {
+  let encrypKeys = Object.keys(encryption);
+  let unique = [];
 
-// console.log(decoded)
+  let newData = data.map(card => {
+    let cardKeys = Object.keys(card);
+
+    cardKeys.forEach(key => {
+
+      // check exceptions to proceed decoding
+      if (!excluded.includes(key)) {
+
+        // get value(id) & check translations
+        if (!encrypKeys.includes(card[key])) {
+          // id is unique
+          unique.push(card[key]);
+        } else {
+          // id can be encoded
+          console.log('card[key]', card[key]);
+          console.log('encryption[key]', encryption[card[key]]);
+          card[key] = encryption[card[key]];
+        }
+
+      }
+      
+    });
+
+    return card;
+
+  });
+
+  uniqueIds = [...new Set(unique)];
+  return newData;
+}
+
+// let encoding begin!
+let decoded = decode(encoded, translations);
+
+console.log('decoded', decoded);
+console.log('unique', uniqueIds);
